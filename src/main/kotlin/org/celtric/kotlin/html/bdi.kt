4e977eef59f0517432b86a08deb39151bae3cd5d
Ricard Osorio <celtric@gmail.com>
2018-02-15 22:29:29 +0100

    Add batch of new elements

--- src/main/kotlin/org/celtric/kotlin/html/bdi.kt
+++ src/main/kotlin/org/celtric/kotlin/html/bdi.kt
@@ -1,3 +1,23 @@
 package org.celtric.kotlin.html
 
-// TODO
+fun bdi(
+    // Optional
+    dir: String? = null,
+
+    // Global
+    css: String? = null,
+    id: String? = null,
+    title: String? = null,
+
+    // Custom
+    other: Attributes = emptyMap(),
+    data: Attributes = emptyMap(),
+
+    // Content
+    content: () -> Any
+) = InlineElement("bdi", content(), AllAttributes(mapOf(
+    "dir" to dir,
+    "class" to css,
+    "id" to id,
+    "title" to title
+), other, data))
--- src/main/kotlin/org/celtric/kotlin/html/bdo.kt
+++ src/main/kotlin/org/celtric/kotlin/html/bdo.kt
@@ -1,3 +1,23 @@
 package org.celtric.kotlin.html
 
-// TODO
+fun bdo(
+    // Mandatory
+    dir: String,
+
+    // Global
+    css: String? = null,
+    id: String? = null,
+    title: String? = null,
+
+    // Custom
+    other: Attributes = emptyMap(),
+    data: Attributes = emptyMap(),
+
+    // Content
+    content: () -> Any
+) = InlineElement("bdo", content(), AllAttributes(mapOf(
+    "dir" to dir,
+    "class" to css,
+    "id" to id,
+    "title" to title
+), other, data))
